c8cc489f568a4dfee1aed655602ae59647fa2d6f
bmoyeon <60872814+bmoyeon@users.noreply.github.com>
2020-08-03 15:08:15 +0900

    [add] word detail view

--- word/urls.py
+++ word/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     MenuView,
     SubWordListView,
-    MainWordListView
+    MainWordListView,
+    WordDetailView,
 )
 
 urlpatterns = [
     path('', MenuView.as_view()),
     path('/sub-list', SubWordListView.as_view()),
     path('/main-list', MainWordListView.as_view()),
+    path('/<int:word_id>', WordDetailView.as_view()),
 ]
